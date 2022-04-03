05305da59dc496dabba724f702fd08ff5a0a0f39
dilarayldzz <86854013+adnanyildiz@users.noreply.github.com>
2022-04-03 18:05:25 -0500

    project started

---
+++ src/test/java/com/ciko/FirstClass.java
@@ -0,0 +1,23 @@
+package com.ciko;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        String str = "Adnan";
+
+        System.out.println("reverse(str) = " + reverse(str));
+
+    }
+
+    public static String reverse (String str){
+        String newstr = "";
+        for (int i = str.length()-1; i >=0; i--) {
+            newstr+=str.charAt(i);
+
+        }
+
+        return newstr;
+    }
+
+}
